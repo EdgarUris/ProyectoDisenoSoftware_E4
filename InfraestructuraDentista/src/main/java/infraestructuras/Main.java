4de192eb74c8bb584b738afbb1dece280fc6bb20
Edgar Urias <edgar.urias261147@potros.itson.edu.mx>
2026-05-11 17:33:39 -0700

    proyecto de infraestructura agregado
    sistema de correo agregado, aun debemos ocultar las credenciales

---
+++ InfraestructuraDentista/src/main/java/infraestructuras/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package infraestructuras;
+
+/**
+ *
+ * @author EdgarUris
+ * @author JeniferFl
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SistemaCorreoElectronico sce = new SistemaCorreoElectronico();
+        sce.enviarCorreo("jenifermaribelalamea@gmail.com", "mamasota", "rica te amo vengase pa ca mija y mamele mami");
+    }
+    
+}
